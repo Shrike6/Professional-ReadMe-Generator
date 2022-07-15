10524426a283d03bcf83221e87157dd917876811
Shrike6 <cody.stubbs@gmail.com>
2022-07-14 21:24:36 -0500

    added function to initialize the readme generator

--- index.js
+++ index.js
@@ -67,11 +67,16 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((inquirerResponses) => {
+        console.log('Creating README...');
+        writeToFile('README.md', generateMarkdown({ ...inquirerResponses }));
+      });
+}
 
 // Function call to initialize app
 init();
